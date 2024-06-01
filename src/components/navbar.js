5779a01e19f43d0f595a81b2576792445dafb59b
Vivian Feng <vivianfeng@Vivians-MacBook-Air-9.local>
2024-05-31 17:25:41 -0700

    navBar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import "../index.css";
 //import { Link } from 'react-router-dom'; // Assuming you are using react-router for navigation
 
+
 export function NavBar() {
-  return (
-    <nav className="navbar">
-      <section className="nav-sidebar">
-        <button className="nav-button">Daily Check-in</button>
-        <button className="nav-button">Your Puppy</button>
-        <button className="nav-button">Mood Log</button>
-        <button className="nav-button">Puppy List</button>
-      </section>
-    </nav>
-  );
+return (
+<nav id="navBar">
+<button className="navButtonSmall">Daily Check-in</button>
+<button className="navButtonSmall">Your Puppy</button>
+<button className="navButtonSmall">Mood Log</button>
+<button className="navButtonSmall">Puppy List</button>
+</nav>
+);
 }
