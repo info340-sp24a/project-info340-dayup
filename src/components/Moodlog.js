9967f1dec7f0ce593035fd18fb4dd198e5cc4d59
Millor-Lei <haichenl@uw.edu>
2024-06-04 04:14:22 -0700

    new

--- src/components/Moodlog.js
+++ src/components/Moodlog.js
@@ -2,49 +2,51 @@ import { NavBar } from './navbar';
 import { PageFooter } from "./footer";
 import React, { useState } from 'react';
 import moodlogData from '../data/moodlogdata-mock.json';
-//import '././index.css';
+import { PageHeader } from "./Header";
+
 function formatDate(dateString) {
   const options = { month: 'long', day: 'numeric' };
   return new Date(dateString).toLocaleDateString('en-US', options);
 }
-
 export function MoodLog() {
-    const [entries, setEntries] = useState(moodlogData);
-    
-    const getIcon = (type, value) => {
-      if (type === 'mood') {
-            return `img/emojis/color-face${value}.png`;
-        } else if (type === 'sleep' || type === 'motivation') {
-            return `img/emojis/color-circle${value}.png`;
-        }
-        return '';
-    }
+  const [entries, setEntries] = useState(moodlogData);
 
-    const renderEntries = () => {
-      return entries.map((entry, index) => (
-        <div key={index} className="mood-tracker-item">
-          <div className="date-box">
-            <span>{formatDate(entry.date)}</span>
+  const renderEntries = () => {
+    return entries.map((entry, index) => (
+      <div key={index} className="mood-tracker-item">
+        <div className="date-box">
+          <span>{formatDate(entry.date)}</span>
+        </div>
+        <div className="mood-details">
+          <div className="mood-icon">
+            < img src={`img/emojis/color-face${entry.mood}.png`} alt="Mood"/><br />
+            <span>Mood</span>
           </div>
-          <div className="mood-details">
-            <div className="mood-icon">
-              <img src={getIcon('mood', entry.mood)} alt="Mood" />
-              <span>Mood</span>
-            </div>
-            <div className="sleep-icon">
-              <img src={getIcon('sleep', entry.sleep)} alt="Sleep" />
-              <span>Sleep</span>
-            </div>
-            <div className="motivation-icon">
-              <img src={getIcon('motivation', entry.motivation)} alt="Motivation" />
-              <span>Motivation</span>
-            </div>
+          <div className="sleep-icon">
+            < img src={`img/emojis/color-circle${entry.sleep}.png`} alt="Sleep"/><br />
+            <span>Sleep</span>
           </div>
-          <div className="note-box">
-            <p>{entry.daily_note}</p>
+          <div className="motivation-icon">
+            < img src={`img/emojis/color-circle${entry.motivation}.png`} alt="Motivation"/><br />
+            <span>Motivation</span>
           </div>
         </div>
-      ));
-    };
-    
+        <div className="note-box">
+          <p>{entry.daily_note}</p >
+        </div>
+      </div>
+    ));
+  };
+
+  return (
+    <body>
+      <NavBar />
+      <main>
+        <section className="moodlog-container">
+          {renderEntries()}
+        </section>
+      </main>
+      <PageFooter />
+    </body>
+  );
 }
